a2b54ff04da27e9f1dbd8000d629aa2e7678d4ca
wesleygoyette <wesley.goyette@gmail.com>
2022-12-30 14:49:33 -0700

    adding functionaility

--- sketch.js
+++ sketch.js
@@ -1,12 +1,10 @@
-function setup() {
-  createCanvas(400, 400);
+function setup(){
+
+    createCanvas(windowWidth,windowHeight);
+    noStroke();
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function draw(){
+
+  background(random(255),random(255),random(255));
 }
